acc41a96bf8a2ccb86094e3c45cca819061a1bb5
tubagusdev90 <tubagusdev@gmail.com>
2025-11-22 20:46:18 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,35 +1,3 @@
-// <?php
-
-// namespace Database\Seeders;
-
-// use App\Models\User;
-// use Illuminate\Database\Seeder;
-// use Illuminate\Support\Facades\Hash;
-
-// class UserSeeder extends Seeder
-// {
-//     /**
-//      * Jalankan seeder.
-//      */
-//     public function run(): void
-//     {
-//         // Hapus user lama agar tidak duplikat (opsional, hanya untuk testing/dev)
-//         User::where('email', 'tubagusdev@gmail.com')->delete();
-
-//         // Buat user default / admin
-//         User::create([
-//             'name' => 'Tubagus Mochamad Isnaeni',
-//             'email' => 'tubagusdev@gmail.com',
-//             'password' => Hash::make('admin'), // Gunakan Hash agar sesuai dengan login
-//         ]);
-
-//         // (Opsional) Tambahkan beberapa dummy user lainnya
-//         User::factory()->count(5)->create();
-//     }
-// }
-
-
-
 <?php
 
 namespace Database\Seeders;
@@ -53,13 +21,13 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
         ]);
 
-        // // Membuat akun kedua
-        // User::create([
-        //     'name' => 'Regular User',
-        //     'email' => 'user@example.com',
-        //     'password' => Hash::make('password123'),
-        //     'email_verified_at' => now(),
-        // ]);
+        // Membuat akun kedua
+        User::create([
+            'name' => 'Tubagus Mochamad Isnaeni',
+            'email' => 'tubagusdev@gmail.com',
+            'password' => Hash::make('admin'),
+            'email_verified_at' => now(),
+        ]);
     }
 }
 
