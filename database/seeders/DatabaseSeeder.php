524246086898236633219a16ed7dc508f55bf792
tubagusdev90 <tubagusdev@gmail.com>
2025-12-10 23:30:46 +0700

    Reorder UserSeeder call in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call(DepartemenTeamSeeder::class);
-        $this->call(UserSeeder::class);
         $this->call(PositionSeeder::class);
         $this->call(KaryawanSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
